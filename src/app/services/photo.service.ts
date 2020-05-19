b74e04814ddbb8a1213d4a8e596025419f2cfe29
Fred Morrison <fpmorrison@verizon.net>
2020-05-19 16:17:39 -0400

    after fork

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -9,7 +9,7 @@ const { Camera, Filesystem, Storage } = Plugins;
 })
 export class PhotoService {
   public photos: Photo[] = [];
-  private PHOTO_STORAGE: string = "photos";
+  private PHOTO_STORAGE = 'photos';
   private platform: Platform;
 
   constructor(platform: Platform) {
@@ -24,13 +24,13 @@ export class PhotoService {
     // If running on the web...
     if (!this.platform.is('hybrid')) {
       // Display the photo by reading into base64 format
-      for (let photo of this.photos) {
+      for (const photo of this.photos) {
         // Read each saved photo's data from the Filesystem
         const readFile = await Filesystem.readFile({
             path: photo.filepath,
             directory: FilesystemDirectory.Data
         });
-      
+
         // Web platform only: Save the photo into the base64 field
         photo.base64 = `data:image/jpeg;base64,${readFile.data}`;
       }
@@ -39,10 +39,10 @@ export class PhotoService {
 
   /* Use the device camera to take a photo:
   // https://capacitor.ionicframework.com/docs/apis/camera
-  
+
   // Store the photo data into permanent file storage:
   // https://capacitor.ionicframework.com/docs/apis/filesystem
-  
+
   // Store a reference to all photo filepaths using Storage API:
   // https://capacitor.ionicframework.com/docs/apis/storage
   */
@@ -53,7 +53,7 @@ export class PhotoService {
       source: CameraSource.Camera, // automatically take a new photo with the camera
       quality: 100 // highest quality (0 to 100)
     });
-    
+
     const savedImageFile = await this.savePicture(capturedPhoto);
 
     // Add new photo to Photos array
@@ -63,9 +63,9 @@ export class PhotoService {
     Storage.set({
       key: this.PHOTO_STORAGE,
       value: this.platform.is('hybrid')
-              ? JSON.stringify(this.photos)  
+              ? JSON.stringify(this.photos)
               : JSON.stringify(this.photos.map(p => {
-                // Don't save the base64 representation of the photo data, 
+                // Don't save the base64 representation of the photo data,
                 // since it's already saved on the Filesystem
                 const photoCopy = { ...p };
                 delete photoCopy.base64;
@@ -97,7 +97,7 @@ export class PhotoService {
       };
     }
     else {
-      // Use webPath to display the new image instead of base64 since it's 
+      // Use webPath to display the new image instead of base64 since it's
       // already loaded into memory
       return {
         filepath: fileName,
@@ -122,7 +122,7 @@ export class PhotoService {
       const response = await fetch(cameraPhoto.webPath!);
       const blob = await response.blob();
 
-      return await this.convertBlobToBase64(blob) as string;  
+      return await this.convertBlobToBase64(blob) as string;
     }
   }
 
@@ -146,7 +146,7 @@ export class PhotoService {
   }
 
   convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
-    const reader = new FileReader;
+    const reader = new FileReader();
     reader.onerror = reject;
     reader.onload = () => {
         resolve(reader.result);
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+	selector: 'app-tab1',
+	templateUrl: 'tab1.page.html',
+	styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
-
-  constructor() {}
-
+export class Tab1Page implements OnInit {
+	angularVersion = '';
+	constructor() {}
+	ngOnInit(): void {
+		this.angularVersion = VERSION.full;
+	}
 }
